f4ecd66d2e2920c12da75beff75ad760007559ae
mrAYush1804 <ayushtiwari7473@gmail.com>
2024-12-10 18:52:13 +0530

    isme mene text main rainbow colour apply kiya ha edittext ke colour main aur kuch chizo ko permiter define kkiya ha

--- app/src/main/java/com/example/firstcomposeapp/components/Editetextwithiconprectcetwo.kt
+++ app/src/main/java/com/example/firstcomposeapp/components/Editetextwithiconprectcetwo.kt
@@ -1,15 +1,16 @@
 package com.example.firstcomposeapp.components
 
-import android.text.InputType
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -21,8 +22,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -32,41 +37,46 @@ fun custom_edittxt(
     placeholder: String,
     value: String,
     onvaluechange: (String) -> Unit,
-    inputType: KeyboardType= KeyboardType.Text,
-    modifier: Modifier
+    inputType: KeyboardType = KeyboardType.Text,
+    visualTransformation: PasswordVisualTransformation? = null,
+    isPasswordField: Boolean = false,
+    modifier: Modifier,
+
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null
 ) {
     val isTextEmpty = value.isEmpty()
+    val rainbowColors = listOf(Color.Red, Color.Yellow, Color.Green, Color.Blue)
+
+
+    val brush = remember {
+        Brush.linearGradient(
+            colors = rainbowColors
+        )
+    }
+
     OutlinedTextField(
         label = { Text(lable) },
         placeholder = { Text(placeholder) },
         value = value,
         onValueChange = onvaluechange,
-        modifier = modifier.border(
-            BorderStroke(
-                width = 3.dp,
-                brush = Brush.sweepGradient(listOf(Color.Green,Color.Black))
-            ),
-            shape = CutCornerShape(6.dp)
-        ).padding(horizontal = 6.dp), keyboardOptions = KeyboardOptions(
+        maxLines = 1,
+        textStyle = TextStyle(brush = brush),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp), keyboardOptions = KeyboardOptions(
             keyboardType = inputType,
             imeAction = ImeAction.Done
+        ), leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        visualTransformation = if (isPasswordField) PasswordVisualTransformation() else VisualTransformation.None,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedTextColor = Color.Transparent,
+            unfocusedTextColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
         ),
 
-        colors = OutlinedTextFieldDefaults.colors().copy(
-
-            focusedContainerColor = Color.Transparent,
-            focusedTextColor = Color.Black,
-            focusedLabelColor = if (isTextEmpty) Color.Black else Color.Transparent,
-            unfocusedLabelColor = if (isTextEmpty) Color.Black else Color.Transparent,
-            unfocusedTextColor = Color.Black,
-            unfocusedContainerColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-
-
-
-
-        )
 
 
     )
@@ -81,14 +91,24 @@ fun showedittext() {
     var Email by remember { mutableStateOf("") }
 
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp).padding(horizontal = 16.dp)
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                brush = Brush.horizontalGradient(listOf(Color.Yellow, Color.Gray)),
+                shape = RectangleShape,
+            )
+            .padding(16.dp)
+            .padding(horizontal = 16.dp)
     ) {
         custom_edittxt(
             lable = "Name",
             placeholder = "Enter your first name",
             value = fname,
-            onvaluechange ={fname=it},
-            modifier = Modifier.fillMaxWidth()
+            onvaluechange = { fname = it },
+            modifier = Modifier.fillMaxWidth(),
+            leadingIcon = { Icon(imageVector = Icons.Filled.Person, " hii") }
 
         )
         Spacer(modifier = Modifier.padding(10.dp))
@@ -96,8 +116,9 @@ fun showedittext() {
             lable = "latname",
             placeholder = "Enter your last name",
             value = lname,
-            onvaluechange ={lname=it},
-            modifier = Modifier.fillMaxWidth()
+            onvaluechange = { lname = it },
+            modifier = Modifier.fillMaxWidth(),
+            leadingIcon = { Icon(imageVector = Icons.Filled.Person, " hii") }
 
         )
         Spacer(modifier = Modifier.padding(10.dp))
@@ -105,8 +126,9 @@ fun showedittext() {
             lable = "Email",
             placeholder = "Enter your Email",
             value = Email,
-            onvaluechange ={Email=it},
-            modifier = Modifier.fillMaxWidth()
+            onvaluechange = { Email = it },
+            modifier = Modifier.fillMaxWidth(),
+            leadingIcon = { Icon(imageVector = Icons.Filled.Email, " hii") }
 
         )
         Spacer(modifier = Modifier.padding(10.dp))
@@ -114,12 +136,15 @@ fun showedittext() {
             lable = "phone",
             placeholder = "Enter your phoneno",
             value = phoneno,
-            onvaluechange ={phoneno=it},
-            modifier = Modifier.fillMaxWidth()
+            onvaluechange = { phoneno = it },
+            isPasswordField = true,
+            modifier = Modifier.fillMaxWidth(),
+            inputType = KeyboardType.Phone,
+            leadingIcon = { Icon(imageVector = Icons.Filled.Phone, " hii") }
+
 
         )
     }
-
 
 
 }
